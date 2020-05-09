68ffa544c135bb65567c6ba8b8da6d83894a130a
wlladimirboichuk <wlladimirboichuk@gmail.com>
2020-05-09 17:19:06 +0300

    Add action

--- UiButton/ViewController.swift
+++ UiButton/ViewController.swift
@@ -11,8 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var label: UILabel!
-    
-
+    @IBOutlet var actionButtons: [UIButton]!
     @IBOutlet weak var button: UIButton!
     
     override func viewDidLoad() {
@@ -24,19 +23,45 @@ class ViewController: UIViewController {
         //Цвет шрифта.
         label.textColor = UIColor.red
         
+        button.isHidden = true
+        
+        for button in actionButtons {
+            button.setTitleColor(.blue, for: .normal)
+            button.backgroundColor = .green
+        }
         //Для аутлета и кнопки.
-        button.setTitle("Get result", for: .normal)
-        button.setTitleColor(.blue, for: .normal)
-        button.backgroundColor = .green
+        button.setTitle("Clear", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .red
     }
 
     @IBAction func pressedButton(_ sender: UIButton) {
-        label.isHidden = false 
-        label.text = "Hello!"
-    }
+        
+        label.isHidden = false
+        button.isHidden = false
+        
+        if sender.tag == 0 {
+            label.text = "hi,world"
+            label.textColor = .red
+        } else if sender.tag == 1 {
+            label.text = "Hi!"
+            label.textColor = .blue
+        } else if sender.tag == 2 {
+            button.isHidden = true 
+        }
+//        if label.isHidden {
+//        label.isHidden = false
+//        label.text = "Hello!"
+//            button.setTitle("clear", for: UIControl.State.normal)
+//            button.setTitleColor(.white, for: .normal)
+//            button.backgroundColor = .red
+//        } else {
+//        label.isHidden = true
+//        button.setTitle("Get result", for: .normal)
+//        button.setTitleColor(.blue, for: .normal)
+//        button.backgroundColor = .green
     
     
     }
     
-    
-
+}
